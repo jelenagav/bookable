30c5f7fb04975be9cfb7cfa172e7e70716618ad5
Changhun Yun <isaac.c.yun@gmail.com>
2019-02-26 16:09:10 +0100

    homepage done with search bar and list icon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+
+function expand() {
+  $(".search").toggleClass("close");
+  $(".input").toggleClass("square");
+  if ($('.search').hasClass('close')) {
+    $('input').focus();
+  } else {
+    $('input').blur();
+  }
+}
+$('button').on('click', expand);
