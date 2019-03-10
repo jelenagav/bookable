1fd1e987df0725896d37f16b6d7a9ee4aa712789
David Bordeleau <davidbordeleau@hotmail.com>
2019-03-10 13:45:54 +0100

    add relevent seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => :registrations }
   root to: 'pages#home'
+  get 'home', to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     resources :books, except: [:create]
   end
   resources :books, only: [:index, :show, :create, :destroy] # /books
   resources :bookings
-
-
 end
